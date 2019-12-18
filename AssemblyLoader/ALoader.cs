032df53b522e26e4ff322dbca3892b49fbb4ae76
KaivnD <KaivnD@hotmail.com>
2019-12-18 18:35:30 +0800

    add:Loader

---
+++ AssemblyLoader/ALoader.cs
@@ -0,0 +1,20 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLoader
+{
+    public class ALoader : GH_AssemblyPriority
+    {
+        public override GH_LoadingInstruction PriorityLoad()
+        {
+            string refdll = @"D:\Desktop\KVDT\KVDT\bin\Debug\KVDT.dll";
+            Assembly.LoadFrom(refdll);
+            return GH_LoadingInstruction.Proceed;
+        }
+    }
+}
